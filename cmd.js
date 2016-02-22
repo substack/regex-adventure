4c0a591d05c443b5ff89e8184e4a91d32bafedff
Denis Zavgorodny <denis.zavgorodny@gmail.com>
2016-02-23 01:14:07 +0200

    all

--- cmd.js
+++ cmd.js
@@ -10,14 +10,14 @@ var lessons = [
   { title: '4 Пiдмножини', file: './problems/char_class' }, //done
   { title: '5 Заперечення та пiдмножини', file: './problems/negated_char_class' }, //done
   { title: '6 Метасимволи', file: './problems/meta' }, //done
-  { title: '7 Квантiфiкатори', file: './problems/quantifier' }, //done
+  { title: '7 Квантифiкатори', file: './problems/quantifier' }, //done
   { title: '8 Роздiлення', file: './problems/split' }, //done
   { title: '9 Захват', file: './problems/capture' }, //done
   { title: '10 Захват з пошуком кiнця слова', file: './problems/capture_breakword' }, //done
-  { title: '11 Оператор альтернативи (логiчне або)', file: './problems/alteration' },
-  { title: 'QUANTIFIED GROUP', file: './problems/quantified_group' },
-  { title: 'QUOTES', file: './problems/quotes' },
-  { title: 'BLINK', file: './problems/blink' }
+  { title: '11 Оператор альтернативи (логiчне або)', file: './problems/alteration' }, //done
+  { title: '12 Групи квантифiкаторiв', file: './problems/quantified_group' }, //done
+  { title: '13 Лапки в регулярних виразах', file: './problems/quotes' }, //done
+  { title: '14 Замiна з групами', file: './problems/blink' }
 ]
 lessons.forEach(function (lesson) {
   shop.add(lesson.title, function () { return require(lesson.file) })
